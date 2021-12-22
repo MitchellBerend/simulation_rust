fe0886bbf3e7d7b16c8ec62eaf06a5b7ec4f47fe
Mitchell Berendhuysen <mitchellbhuysen@hotmail.com>
2021-12-22 12:37:52 +0100

    added documentation

--- src/functions.rs
+++ src/functions.rs
@@ -8,24 +8,62 @@
 //! use sim_rust::Agent;
 //! use sim_rust::functions::*;
 //!
-//! //method 1 and 2 have the exact same outcome 
+//! // method 1 and 2 have the exact same outcome 
 //! 
-//! //method 1  
-//! let mut env_1 = generate_default_env::<ExampleAgent>(10).unwrap();
+//! // method 1  
+//! let mut env_1 = generate_default_env::<ExampleAgent1>(10).unwrap();
 //! env_1 = tick(env_1).unwrap(); //(or tick_collect)
 //! collect(env_1);
 //!
-//! //method 2
-//! let mut env_2 = generate_default_tick_collect::<ExampleAgent>(10, 1, 1).unwrap();
+//! 
+//! // method 2
+//! let mut env_2 = generate_default_tick_collect::<ExampleAgent1>(10, 1, 1).unwrap();
 //! 
 //! 
-//!  
+//! // method 3 
+//! let mut env_3 = generate_default_env::<ExampleAgent1>(10).unwrap();
+//! // Any type that implements the Agent trait can be added to the environment.
+//! let example_agent_2 = ExampleAgent2::generate().unwrap();
+//! env_3.add_agent(example_agent_2).unwrap();
+//! // even mulitple instances at once.
+//! let example_agents: Vec<Box<dyn Agent>> = vec![
+//!     ExampleAgent2::generate().unwrap(),
+//!     ExampleAgent2::generate().unwrap(),
+//!     ExampleAgent2::generate().unwrap(),
+//! ];
+//! env_3.add_agents(example_agents).unwrap();
+//! env_3 = tick(env_3).unwrap();
+//! collect(env_3);
+//! 
+//!   
 //! // This is just a very simple implementation of an agent. 
-//! struct ExampleAgent {
+//! struct ExampleAgent1 {
 //!     age: u8,
 //! }
 //! 
-//! impl Agent for ExampleAgent {
+//! struct ExampleAgent2 {
+//!     age: u8,
+//! }
+//! 
+//! 
+//! impl Agent for ExampleAgent1 {
+//!     fn generate() -> Result<Box<Self> ,&'static str> {
+//!         let agent = Box::new(Self {age: 1});
+//!         Ok(agent)
+//!     }
+//! 
+//!     fn tick(&mut self) -> Result<(), &'static str> {
+//!         self.age += 1;
+//!         Ok(())
+//!     }
+//!
+//!     fn collect(&self)  -> Result<(), &'static str> {
+//!         Ok(())
+//!     }
+//! }
+//! 
+//! // This is a direct copy of the implementation of ExampleAgent1
+//! impl Agent for ExampleAgent2 {
 //!     fn generate() -> Result<Box<Self> ,&'static str> {
 //!         let agent = Box::new(Self {age: 1});
 //!         Ok(agent)
